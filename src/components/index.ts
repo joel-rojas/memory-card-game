acb8e25b1c569c0505f4788ff1d986fb6ee64b13
Emerson Rojas <joel.rojas.soliz@gmail.com>
2023-06-18 16:51:53 -0400

    Add initial config for UI components

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export * from './board';
+export * from './body';
+export * from './button';
+export * from './card';
+export * from './countdown';
+export * from './level';
+export * from './header';
