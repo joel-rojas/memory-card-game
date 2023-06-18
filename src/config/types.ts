dc5eb71daa2f4b1b4455735f7cf5abd4e74f32f5
Emerson Rojas <joel.rojas.soliz@gmail.com>
2023-06-18 16:53:01 -0400

    Add initial reusable types for the app

--- src/config/types.ts
+++ src/config/types.ts
@@ -1,15 +1,22 @@
+export interface MCSingleComponentProps {
+  children: React.ReactNode;
+}
+
 export interface MCAppState {
   gameLevel: MCGameLevel;
-  gameCards: MCGameCardList;
   gameStatus: MCGameStatus;
   currentCountdown: number;
+}
+
+export interface MCGameState {
+  cardDeck: MCGameCardDeck;
   cardsShown: MCGameCardsShown;
-};
+}
 
 export type MCGameCardsShown = 2 | 1 | 0;
 
 export interface MCGameLevel {
-  label: 'easy' | 'normal' | 'hard';
+  label: "easy" | "normal" | "hard";
   countdown: 45 | 30 | 15;
 }
 
@@ -19,7 +26,6 @@ export type MCGameCard = {
   isMatched: boolean;
 };
 
-export type MCGameCardList = MCGameCard[];
+export type MCGameCardDeck = MCGameCard[];
 
-
-export type MCGameStatus = 'idle' | 'inProgress' | 'lose' | 'win';
+export type MCGameStatus = "idle" | "inProgress" | "lose" | "win";
