ac6e8288c42f4ca04e1bad5ad25e0496292326cd
Emerson Rojas <joel.rojas.soliz@gmail.com>
2023-10-04 00:28:00 -0400

    Build countdown+header UI component and logic

--- src/components/countdown/countdown.component.tsx
+++ src/components/countdown/countdown.component.tsx
@@ -1,5 +1,15 @@
 import React from "react";
 
-export default function TimeSlot() {
-  return <div></div>;
+interface CountDownProps {
+  children: React.ReactNode;
 }
+
+const CountDown: React.FC<CountDownProps> = ({ children: time }) => {
+  let parsedTime = "00";
+  if (typeof time === "number" && time >= 0) {
+    parsedTime = (time as number) < 10 ? `0${time}` : `${time}`;
+  }
+  return <h1 className="sm:text-sm lg:text-lg">{parsedTime}</h1>;
+};
+
+export default CountDown;
