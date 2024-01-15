c820ba8ff552eba031139e78b2bbe31941a64b8a
Emerson Rojas <joel.rojas.soliz@gmail.com>
2024-01-15 00:17:42 -0400

    Add new TS types for Modal

--- src/config/types.ts
+++ src/config/types.ts
@@ -2,6 +2,24 @@ export interface MCSingleComponentProps {
   children: React.ReactNode;
 }
 
+export interface MCGameLevel {
+  label: "easy" | "normal" | "hard";
+  countdown: 45 | 30 | 15;
+}
+
+export type MCGameCard = {
+  id: string;
+  uid: string;
+  isHidden: boolean;
+  isMatched: boolean;
+};
+
+export type MCGameCardDeck = MCGameCard[];
+
+export type MCGameProgress = "idle" | "inProgress" | "lose" | "win";
+
+export type MCGameStatus = "new" | "reset" | "resume" | "pause" | "end";
+
 export interface MCAppState {
   gameLevel: MCGameLevel;
   gameStatus: MCGameStatus;
@@ -22,20 +40,47 @@ export type MCGameSelectedCards = {
   [key: MCGameCard["uid"]]: MCGameCard;
 };
 
-export interface MCGameLevel {
-  label: "easy" | "normal" | "hard";
-  countdown: 45 | 30 | 15;
-}
+export type MCGameChoosenModalType<T> = T extends MCGameStatus ? T : never;
 
-export type MCGameCard = {
-  id: string;
-  uid: string;
-  isHidden: boolean;
-  isMatched: boolean;
+export type MCGameModalType = {
+  type: MCGameChoosenModalType<"end" | "pause">;
+  isShown: boolean;
 };
 
-export type MCGameCardDeck = MCGameCard[];
+export type MCGameUITypes = "button" | "title";
 
-export type MCGameProgress = "idle" | "inProgress" | "lose" | "win";
+export type MCGameChoosenUIType<T> = T extends MCGameUITypes ? T : never;
 
-export type MCGameStatus = "new" | "reset" | "resume" | "pause";
+export type MCGamePropsSet<T> = T;
+
+export type MCGameCurrentUIProps =
+  | MCGamePropsSet<MCGameButtonProps>
+  | MCGamePropsSet<MCGameHeadlineProps>;
+
+export type MCGameUIPropsList = MCGameCurrentUIProps[];
+
+export type MCGameCurrentModalActionKeys =
+  | "title"
+  | "resume"
+  | "reset"
+  | "mainMenu";
+
+export type MCGameUISetPropsMap = {
+  [P in keyof Record<
+    MCGameCurrentModalActionKeys,
+    unknown
+  >]: MCGameCurrentUIProps;
+};
+
+export interface MCGameButtonProps {
+  text: string;
+  type: MCGameChoosenUIType<"button">;
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  btnCls?: string;
+};
+
+export interface MCGameHeadlineProps {
+  text: string;
+  type: MCGameChoosenUIType<"title">;
+  size: "large" | "x-large";
+};
