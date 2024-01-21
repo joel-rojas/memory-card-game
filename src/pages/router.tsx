0d0d73c5338da1e79ab3d719d9b531f9dd0ff551
Emerson Rojas <joel.rojas.soliz@gmail.com>
2024-01-21 13:42:19 -0400

    Redirect to home page when user hits on main menu button from UI modal by adding a new reusable route path name enum and using navigate method from useNavigate hook

--- src/pages/router.tsx
+++ src/pages/router.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 
 import { Home, NotFound, Play } from "@pages";
+import { MCGameRoutePath } from "@config";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Home />, errorElement: <NotFound />},
-  { path: "/play", element: <Play /> },
+  { path: MCGameRoutePath.HOME, element: <Home />, errorElement: <NotFound /> },
+  { path: MCGameRoutePath.PLAY, element: <Play /> },
 ]);
 
 export default router;
