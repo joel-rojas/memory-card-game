1db7ee8328a1c2fc5e9f132054c8214a20af0605
Emerson Rojas <joel.rojas.soliz@gmail.com>
2024-02-18 18:38:37 -0400

    Implement new reusable UI radio group and radio buttons for progressive menu shown on Home page

---
+++ src/components/radio/radio.component.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import { MCGameRadioButtonProps } from "@config";
+
+const RadioButton: React.FC<MCGameRadioButtonProps> = ({
+  name,
+  value,
+  checked,
+  label,
+  onChange,
+}) => {
+  return (
+    <div className="flex justify-start p-1 items-center cursor-pointer">
+      <input
+        id={`${name}_${value}`}
+        type="radio"
+        value={value}
+        name={name}
+        checked={checked}
+        className="appearance-none"
+        onChange={onChange}
+      ></input>
+
+      <label htmlFor={`${name}_${value}`} className="w-full text-xl cursor-pointer">
+        <span className="pr-1">{checked ? '•' : <>&nbsp;</>}</span>
+        {label}
+      </label>
+    </div>
+  );
+};
+
+export default RadioButton;
