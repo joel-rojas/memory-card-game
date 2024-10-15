8f592555ca82069ac0a8c43fc23042e4f7207ccb
Emerson Rojas <joel.rojas.soliz@gmail.com>
2024-10-14 23:37:53 -0400

    feature: Implement basic unit testing on board UI component

--- src/components/board/board.component.tsx
+++ src/components/board/board.component.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 interface BoardProps {
-  children: React.ReactElement | React.ReactElement[];
+  children: React.ReactNode | React.ReactNode[];
 }
 const Board: React.FC<BoardProps> = ({ children }) => {
   return (
-    <div className="grid grid-cols-4 gap-4 place-content-center md:grid-cols-5">
+    <div id="board" data-testid="board" className="grid grid-cols-4 gap-4 place-content-center md:grid-cols-5">
       {children}
     </div>
   );
--- src/components/board/board.test.tsx
+++ src/components/board/board.test.tsx
@@ -1,1 +1,36 @@
-export {};
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Board from "./board.component";
+
+describe("Board Component", () => {
+  it("renders without crashing", async () => {
+    const { container, rerender } = render(
+      <Board>
+        <></>
+      </Board>
+    );
+    const boardElement = await screen.findByTestId("board");
+    expect(boardElement).toBeInTheDocument();
+    // Test rerender with string as children
+    rerender(<Board>{'Test'}</Board>);
+    expect(container).toContainHTML('Test');
+    // Test rerender with number as children
+    rerender(<Board>{1}</Board>);
+    expect(container).toContainHTML('1');
+    // Test rerender with boolean as children
+    rerender(<Board>{true.toString()}</Board>);
+    expect(container).toContainHTML('true');
+    // Test rerender without children
+    rerender(<Board>{undefined}</Board>);
+    expect(container).toContainHTML('');
+    // Test rerender with multiple children
+    rerender(
+      <Board>
+        <div>Test 1</div>
+        <div>Test 2</div>
+      </Board>
+    );
+    expect(container).toContainHTML('Test 1');
+    expect(container).toContainHTML('Test 2');
+  });
+});
