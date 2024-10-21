9adb27c97cd240faf54a4bd89e82e8301ccb3b55
Emerson Rojas <joel.rojas.soliz@gmail.com>
2024-10-21 00:01:21 -0400

    feature: Add unit tests for UI card component

--- src/components/card/card.component.tsx
+++ src/components/card/card.component.tsx
@@ -1,6 +1,6 @@
-/* eslint-disable jsx-a11y/alt-text */
-import { MCGameCard } from "@config";
 import React from "react";
+
+import { MCGameCard } from "@config";
 
 interface CardProps {
   card: MCGameCard;
@@ -10,10 +10,24 @@ interface CardProps {
   ) => void;
 }
 
-const Card: React.FC<CardProps> = ({ card, onTap }) => {
+/**
+ * Card component represents a single card in the memory card game.
+ *
+ * @component
+ * @param {CardProps} props - The properties for the Card component.
+ * @param {MCGameCard} props.card - The card object containing its properties.
+ * @param {(ev: React.MouseEvent<HTMLDivElement, MouseEvent>, card: MCGameCard) => void} props.onTap - The function to call when the card is clicked.
+ *
+ * @returns {JSX.Element} A JSX element representing the card.
+ *
+ * @example
+ * <Card card={card} onTap={handleCardTap} />
+ */
+const Card: React.FC<CardProps> = ({ card, onTap }: CardProps): JSX.Element => {
   const { isHidden } = card;
   return (
     <div
+      data-testid={`card-${card.uid}`}
       className={`w-16 sm:w-28 md:w-36 lg:w-48 h-16 sm:h-28 md:h-36 lg:h-48 ${
         isHidden ? "cover-card" : "shadow shadow-gray-400"
       } relative rounded-sm cursor-pointer `}
@@ -23,12 +37,14 @@ const Card: React.FC<CardProps> = ({ card, onTap }) => {
         className={`p-1 absolute select-none pointer-events-none ${
           isHidden ? "visible" : "invisible"
         }`}
+        alt={`cover card`}
         src={require(`@assets/cover_card.png`)}
       />
       <img
         className={`p-1 absolute select-none pointer-events-none ${
           isHidden ? "invisible" : "visible"
         }`}
+        alt={`face card`}
         src={card.src}
       />
     </div>
