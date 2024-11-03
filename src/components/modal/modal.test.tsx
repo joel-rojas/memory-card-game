b2e1e8a1472d3c9dcfa192901b159d37382b93be
Emerson Rojas <joel.rojas.soliz@gmail.com>
2024-11-03 12:36:13 -0400

    fix: Remove unused userEvent utility from testing library

--- src/components/modal/modal.test.tsx
+++ src/components/modal/modal.test.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import Modal from "./modal.component";
-import userEvent from "@testing-library/user-event";
 
 describe("Modal component", () => {
   const mockOnClose = jest.fn();
