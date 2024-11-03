b6d305bb1ab07ba12a73e2cbe6cdd260d80fe9cb
Emerson Rojas <joel.rojas.soliz@gmail.com>
2024-11-03 12:33:25 -0400

    feature: Add unit test for modal component

--- src/components/modal/modal.component.tsx
+++ src/components/modal/modal.component.tsx
@@ -31,10 +31,12 @@ const Modal: React.FC<ModalProps> = ({
 
   return isOpen ? (
     <div
+      data-testid="modalBackground"
       className="fixed z-50 left-0 top-0 w-full h-full overflow-auto bg-black/90"
       onClick={handleModalOnBackgroundClick}
     >
       <div
+        role="dialog"
         className="flex flex-col bg-white p-4 my-[10%] mx-auto w-1/2"
         onClick={handleModalContentClick}
       >
