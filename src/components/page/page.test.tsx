76a9b98122eb3a85652e7f5f06aa9d43b82000fb
Emerson Rojas <joel.rojas.soliz@gmail.com>
2024-11-03 12:57:52 -0400

    feature: Unit test for page component

--- src/components/page/page.test.tsx
+++ src/components/page/page.test.tsx
@@ -1,1 +1,15 @@
-export {};
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Page from "./page.component";
+
+describe("Page component", () => {
+  it("renders children correctly", () => {
+    render(
+      <Page>
+        <div>Test Child</div>
+      </Page>
+    );
+    const childElement = screen.getByText(/test child/i);
+    expect(childElement).toBeInTheDocument();
+  });
+});
