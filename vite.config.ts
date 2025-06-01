03e6029e636869040d0b0f89353552bd4e630eb6
Emerson Rojas <joel.rojas.soliz@gmail.com>
2025-05-31 21:18:38 -0400

    chore: Add tsconfig.paths.json for path aliasing and update vite.config.ts to include alias resolution

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@/": './src',
+      "@/components": './src/components',
+      "@/pages": './src/pages',
+      "@/assets": './src/assets',
+      "@/utils": './src/utils',
+      "@/hooks": './src/hooks',
+      "@/contexts": './src/contexts',
+      "@/containers": './src/containers',
+      "@/config": './src/config',
+      "@/store": './src/store',
+    }
+  }
 })
