a1a10da11d4351a6639e6873b61634352554a12c
Emerson Rojas <joel.rojas.soliz@gmail.com>
2025-06-08 22:36:54 -0400

    feature: Append all UI container components from previous app and alter their unit testing files to utilize vitest

---
+++ src/containers/menu-content/menu-content.container.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+import type { MCGameUIPropsList, MCGameCurrentUIProps } from "@/config";
+import { Button, Text, RadioGroup } from "@/components";
+
+interface MenuContentProps {
+  contentList: MCGameUIPropsList;
+  fullWidth?: boolean;
+}
+
+const MenuContent: React.FC<MenuContentProps> = ({
+  contentList,
+  fullWidth = false,
+}): React.ReactElement => {
+  const renderChild = (child: MCGameCurrentUIProps) => {
+    const key = `${child.type}-${child.label}`;
+    switch (child.type) {
+      case "headline":
+      case "paragraph":
+        return (
+          <Text
+            type={child.type}
+            key={key}
+            size={child.size}
+            label={child.label}
+          />
+        );
+      case "button":
+        return (
+          <Button
+            type={child.type}
+            key={key}
+            label={child.label}
+            btnCls={child.btnCls}
+            onClick={child.onClick}
+            textCls={child.textCls}
+          />
+        );
+      case "radio-group":
+        return (
+          <RadioGroup type={child.type} key={key} options={child.options} />
+        );
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div
+      role="presentation"
+      className={`flex flex-col items-center justify-center ${
+        fullWidth ? "w-full" : "w-auto"
+      }`}
+    >
+      {contentList.map(renderChild)}
+    </div>
+  );
+};
+
+export default MenuContent;
---
+++ src/containers/progressive-menu/progressive-menu.container.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import type { MCGameMainMenuContentKeys } from "@/config";
+
+interface ProgressiveMenuProps {
+  renderMenu: (
+    currentMenu: MCGameMainMenuContentKeys,
+    nextMenu: (menuKey: MCGameMainMenuContentKeys) => void
+  ) => React.ReactElement | null;
+  currentMenu: MCGameMainMenuContentKeys;
+  onUpcomingMenu: (menuKey: MCGameMainMenuContentKeys) => void;
+}
+
+const ProgressiveMenu: React.FC<ProgressiveMenuProps> = ({
+  renderMenu,
+  currentMenu,
+  onUpcomingMenu,
+}) => {
+  const nextMenu = (menuKey: MCGameMainMenuContentKeys) => {
+    onUpcomingMenu(menuKey);
+  };
+  if (currentMenu) {
+    return (
+      <div className="flex flex-1 p-4 justify-center items-center bg-white">
+        {renderMenu(currentMenu, nextMenu)}
+      </div>
+    );
+  }
+  return null;
+};
+
+export default ProgressiveMenu;
