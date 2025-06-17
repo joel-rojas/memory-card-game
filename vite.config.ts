23ea5195e7879c86780726baa8b5219baeb5d32c
Emerson Rojas <joel.rojas.soliz@gmail.com>
2025-06-16 23:59:27 -0400

    chore: add Netlify Vite plugin and update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import netlify from "@netlify/vite-plugin";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), netlify()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
